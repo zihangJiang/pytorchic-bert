53d7b1bb600a86bc947700a938c9e92c5bf1d47e
蒋子航 <32448880+zihangJiang@users.noreply.github.com>
2019-11-16 15:38:18 +0800

    Update optim.py
    fix weight decay in param_optimizer to agree with the original (hugging face's) implementation.
    
    (Current implementation seems to apply weight decay to all parameters, since "n not in no_decay" is always True.)

--- optim.py
+++ optim.py
@@ -151,8 +151,8 @@ def optim4GPU(cfg, model):
     param_optimizer = list(model.named_parameters())
     no_decay = ['bias', 'gamma', 'beta']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.0}]
     return BertAdam(optimizer_grouped_parameters,
                     lr=cfg.lr,
                     warmup=cfg.warmup,
